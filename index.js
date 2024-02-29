47ddd65d3b502c9429cd564c20f6f34318430c42
zoeng6project <161369393+zoeng6project@users.noreply.github.com>
2024-02-29 14:52:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+var upload = multer();
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile'),(req,res) =>{
+res.json({
+  name: req.file.originalname,
+  type: req.file.mimetype,
+  size:req.file.size
+})
+})
 
 
 const port = process.env.PORT || 3000;
